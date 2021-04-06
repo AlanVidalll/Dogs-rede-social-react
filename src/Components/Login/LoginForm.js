0459312e6a40adc85d8b19c8eb59a2e849bf2f62
Alan Vidal <alan.fonseca@kumulus.com.br>
2021-04-06 12:38:26 -0300

    Criando funcionalidades de login

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,36 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../Forms/Button";
 import { Input } from "../Forms/Input";
+import useForm from "../../Hooks/useForm.js";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
-  function handleSubmit(event) {
+  const { userlogin, error, loading } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-      });
+
+    if (username.validate() && password.validate()) {
+      userlogin(username.value, password.value);
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usúario" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
-        <Button disabled>Entrar</Button>
+        <Input label="Usúario" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
